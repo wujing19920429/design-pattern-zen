d182f89d1b164d772e6e7b1fd8e9945425ed2e67
wujing <1144165309@qq.com>
2020-08-09 23:45:40 +0800

    feat:基础框架

---
+++ src/main/java/com/wj/subject/IHanFeiZi.java
@@ -0,0 +1,20 @@
+package com.wj.subject;
+
+/**
+ * @Description TODO
+ * @Author wujing
+ * @Date 2020/8/9 23:43
+ * @Since 1.0
+ * @ProjectName design-pattern-zen
+ */
+public interface IHanFeiZi {
+    /**
+     * haveBreackfast
+     */
+    void haveBreackfast();
+
+    /**
+     * haveFun
+     */
+    void haveFun();
+}
